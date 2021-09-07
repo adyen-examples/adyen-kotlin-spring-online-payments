6cac8f61e026d01ff6bb5b8dd40a6e177204e119
Julien Lengrand-Lambert <julien@lengrand.fr>
2021-09-07 09:33:18 +0200

    Add CI

--- src/test/kotlin/com/adyen/checkout/OnlinePaymentsApplicationTests.kt
+++ src/test/kotlin/com/adyen/checkout/OnlinePaymentsApplicationTests.kt
@@ -1,10 +1,29 @@
 package com.adyen.checkout
 
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.springframework.boot.test.context.SpringBootTest
+import java.lang.System.clearProperty
+import java.lang.System.setProperty
 
-@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class OnlinePaymentsApplicationTests {
+
+    @BeforeAll
+    internal fun onceExecutedBeforeAll() {
+        setProperty("ADYEN_API_KEY", "testKey");
+        setProperty("ADYEN_MERCHANT_ACCOUNT", "testAccount");
+        setProperty("ADYEN_CLIENT_KEY", "testKey");
+    }
+
+    @AfterAll
+    internal fun onceExecutedAfterAll(){
+        clearProperty("ADYEN_API_KEY");
+        clearProperty("ADYEN_MERCHANT_ACCOUNT");
+        clearProperty("ADYEN_CLIENT_KEY");
+    }
 
 	@Test
 	fun contextLoads() {
