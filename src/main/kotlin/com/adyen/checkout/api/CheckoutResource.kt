59e42e2c4b3e030530113e54133206014cdbb436
julien Lengrand-Lambert <julien@lengrand.fr>
2022-05-18 17:43:09 +0200

    Sets redirectUrl using hostname (#7)
    Fixes #5

--- src/main/kotlin/com/adyen/checkout/api/CheckoutResource.kt
+++ src/main/kotlin/com/adyen/checkout/api/CheckoutResource.kt
@@ -10,12 +10,10 @@ import com.adyen.service.exception.ApiException
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import java.io.IOException
 import java.util.*
+import javax.servlet.http.HttpServletRequest
 
 
 /**
@@ -38,7 +36,8 @@ class CheckoutResource(@Value("\${ADYEN_API_KEY}") apiKey: String?) {
 
     @PostMapping("/sessions")
     @Throws(IOException::class, ApiException::class)
-    fun sessions(@RequestParam type: String?): ResponseEntity<CreateCheckoutSessionResponse> {
+    fun sessions(@RequestHeader host: String, @RequestParam type: String?, request: HttpServletRequest): ResponseEntity<CreateCheckoutSessionResponse> {
+
         val orderRef = UUID.randomUUID().toString()
         val amount = Amount()
             .currency("EUR")
@@ -47,7 +46,7 @@ class CheckoutResource(@Value("\${ADYEN_API_KEY}") apiKey: String?) {
         checkoutSession.merchantAccount(merchantAccount)
         checkoutSession.channel = CreateCheckoutSessionRequest.ChannelEnum.WEB
         checkoutSession.reference = orderRef // required
-        checkoutSession.returnUrl = "http://localhost:8080/redirect?orderRef=$orderRef"
+        checkoutSession.returnUrl = "${request.scheme}://${host}/redirect?orderRef=$orderRef"
         checkoutSession.amount = amount
         log.info("REST request to create Adyen Payment Session {}", checkoutSession)
         val response = checkout.sessions(checkoutSession)
